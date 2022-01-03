1ccf377ced783e114ae5d48798663b49c6cb744a
Ana Paula Novaes <ana-paula1503@hotmail.com>
2022-01-02 21:43:16 -0300

    add header file

---
+++ so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/02 21:34:29 by apaula-b          #+#    #+#             */
+/*   Updated: 2022/01/02 21:42:43 by apaula-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "./minilibx/mlx.h"
+
+#endif
+
